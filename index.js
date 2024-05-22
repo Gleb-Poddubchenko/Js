b3b61938ea5ca0618eeeeec918994b356c42968f
Glebasta <hlibpoddubchenko@gmail.com>
2024-05-22 23:12:02 +0200

    traning class

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 
-console.log(10,15,"ten");
-console.log(10);
+console.log(window.document);
+console.dir(window.document);
+console.dir(window.document.body);
+console.dir(window.document.body.children[0]);
+const h1 = window.document.body.children[0];
+console.log(h1);
+function handlerClickH1(){
+    console.log('click on h1')
+}
+h1.addEventListener('click',handlerClickH1);
+
+h1.onclick = function () {
+    console.log('onklick')
+}
