0860629bd43c1d2b5fc1eff12ede97e1b7ba1731
Glebasta <hlibpoddubchenko@gmail.com>
2024-05-29 21:40:22 +0200

    traning

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 
-console.log(window.document);
-console.dir(window.document);
-console.dir(window.document.body);
-console.dir(window.document.body.children[0]);
-const h1 = window.document.body.children[0];
-console.log(h1);
-function handlerClickH1(){
-    console.log('click on h1')
-}
-h1.addEventListener('click',handlerClickH1);
+// console.log(window.document);
+// console.dir(window.document);
+// console.dir(window.document.body);
+// console.dir(window.document.body.children[0]);
+// const h1 = window.document.body.children[0];
+// console.log(h1);
+// function handlerClickH1(){
+//     console.log('click on h1')
+// }
+// h1.addEventListener('click',handlerClickH1);
 
-h1.onclick = function () {
-    console.log('onklick')
-}
+// h1.onclick = function () {
+//     console.log('onklick')
+// }
+// console.dir(document)
+// const btns = document.getElementsByTagName('button');
+// console.log(btns);
+
+// // for (const btn of btns) {
+// //     btn.addEventListener('mousemove',()=>{
+// //         console.log(btn);
+// //     })
+// // }
+// const [btn1] = document.getElementsByTagName('button');
+// console.log(btn1);
+// const body = document.body;
+
+// document.getElementById('unique');
+// console.log(unique);
+// const items = document.getElementsByClassName('item');
+// console.log(items);
+
+// const btn2 = document.querySelector('button+button');
+// console.log(btn2);
