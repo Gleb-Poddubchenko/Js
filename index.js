559822134cb1928c6cfb3c17faac8b15046df077
Glebasta <hlibpoddubchenko@gmail.com>
2024-06-24 12:11:55 +0200

    Recurtion

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 "use strict";
-
-const sum1 = function (number1, ...rest) {
-  Array.from(arguments).reduce((summa, elem) => summa + elem);
-};
-
-const sumNumbers2 = (num1, ...rest) => rest.reduce((summa, elem) => summa + elem);
-// собирает в массив все что не имеет своего имени
-const result2 = sumNumbers2(1, 2, 3);
-const result1 = sumNumbers2(1, 2, 3);
+function recursia() {
+  recursia();
+}
 
 
-const arrNums = [5,7,9];
-console.log(arrNums);
-console.log([0],[1],[2]);
-console.log(...arrNums); 
-// srped  разбирает массив на элементы
+function powRecursion (number=2,exp=2){
+  if(exp===0){
+    return 1;
+  }
+  return number * powRecursion(number, exp-1);
+}
+console.log(powRecursion(3,5));
 
-console.log(Math.max (Math.max(7,3,45)));
-console.log(Math.max(...arrNums));
+
+function powRecursion2 (number = 0){
+if(number===0){
+  return 1;
+}
+return number * powRecursion2(number-1);
+}
+console.log(powRecursion2(2));
+
+
 
