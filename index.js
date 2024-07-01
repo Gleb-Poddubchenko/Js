52d3a1a95dd81366e642203f79cacfc1b6dae5df
Glebasta <hlibpoddubchenko@gmail.com>
2024-07-01 23:14:56 +0200

    function consructor

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
 'use strict';
 
-const dataBD = '2000/03/12';
-
-const arrDataBDUser = dataBD.split('/');
-
-
-const now = new Date();
-console.log(now.getFullYear());
-
-const age = now.getFullYear() - Number(dataBD.split('/').at(0));
-console.log(age);
-
-
--- js/myArray.js
+++ js/myArray.js
@@ -1,14 +1,20 @@
 'use strict';
 
+function User(fname,lname,age){
+this.fname = fname;
+this.lname = lname;
+this.age = age;
+}
 
+function Userproto(){
+    this.getFullName = function(){
+        return `${this.fname} ${this.lname}`;
+    }
+}
 
+User.prototype = new Userproto();
 
-
-
-
-
-
-
+const user1 = new User('Tom','Rot',45)
 
 
 
