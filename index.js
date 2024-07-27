a721358ecf41baa4a0baf3eb6db48cae0e2ed665
Glebasta <hlibpoddubchenko@gmail.com>
2024-07-28 01:06:57 +0200

    Map

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 "use strict";
 
-class Queue {
-  constructor() {
-    this._head = 0;
-    this._tail = 0;
-  }
-  get size() {
-    return this._tail - this._head;
-  }
-  enqueue(item) {
-    this[`_${this._tail}`] = item;
-    this._tail++;
-    return this.size;
-  }
-  dequeue() {
-    if (this.size) {
-      const deltItem = [`_${this._head}`];
-      delete this[`_${this._head}`];
-      this._head++;
-      return deltItem;
-    }
-  }
-}
+const map1 = new Map();
+console.log(map1);
+map1.set("bar", "fig");
+map1.set(null, null);
+map1.set(true, 1);
+const sum = (a, b) => a + b;
+map1.set(sum, sum(5, 7));
 
-const queue1 = new Queue();
-console.log(queue1.enqueue(45));
-console.log(queue1.enqueue(46));
-console.log(queue1.enqueue(47));
-console.log(queue1.enqueue(48));
-console.log(queue1);
-console.log(queue1.dequeue());
-console.log(queue1);
-console.log(queue1.enqueue(49));
+console.log( map1.get('bar'))
+
+console.log(map1.has(sum));
+console.log(map1.has(1));
+if(map1.has(null)){
+  map1.delete(null)
+}
+// map1.clear()
+console.log(...map1.entries());
+console.log(map1.keys());
+console.log(map1.values());
+console.log(map1);
+
+
+
+
