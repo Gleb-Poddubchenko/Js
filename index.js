82b68aa9b8a9ecb7451cdff273b796539a5a788f
Glebasta <hlibpoddubchenko@gmail.com>
2024-07-30 19:23:01 +0200

    destrukt

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const arr1 = [4, 5, 9, 4, 8, 6, 9, 4, 9];
+"use strict";
 
-const setArr1 = new Set(arr1);
-console.log(setArr1);
-const uniqArr1 = [...setArr1];
-console.log(uniqArr1);
-const uniqArr2 = [...new Set(arr1)];
-console.log(uniqArr2);
+const user = {
+  public1: 44,
+  privatInfo: {
+    id: 14,
+    login: "Brad",
+    email: "bradgmail.com",
+    bd: {
+      year: 1963,
+      month: 12,
+      day: 18,
+    },
+  },
+  contactInfo: {
+    adress: {
+      town: "NY",
+      street: "15 avenu",
+      house: 547,
+    },
+    phone: "123-56-85",
+  },
+  generalInfo: {
+    profession: "actor",
+    children: ["Fred", "Anna", "Max", "Alex"],
+  },
+};
 
-set1 = new Set();
+console.log(user.privatInfo.login, user.contactInfo.phone);
 
-set1.add(1);
-set1.add(2);
-set1.add(3);
-set1.add(4);
-set1.delete(1);
-console.log(set1);
+//destrur
 
-// console.log(set1.entries());
+const {
+contactInfo:{phone: userPhone,adress:{street}},
+generalInfo:{children:[,,thirdChild]}
+} = user;
 
-console.log(...setArr1.entries());
-console.log([...setArr1.keys()]);
-console.log(...setArr1.values());
-
-console.log(setArr1.has(4));
-
-const sent1 = "Set.prototype";
-const sent2 = "to be or not to be";
-
-const letters = new Set(sent2.split(" "));
-console.log(letters);
-
+console.log(userPhone);
+// console.log(children);
+console.log(street);
+console.log(thirdChild);
