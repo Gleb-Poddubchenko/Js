c06059263bc18905849a4960bb660b31a4558fec
Glebasta <hlibpoddubchenko@gmail.com>
2024-08-12 16:55:09 +0200

    create bc

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 "use strict";
 
-const unique = document.getElementById("unique");
-const [main] = document.getElementsByTagName("main");
-const section = document.querySelector("section");
+const btns = document.querySelectorAll("#wrapper-btn button");
 
-console.log(main.getAttribute("title"));
+console.log(btns);
 
-console.log(main.hasAttribute("button"));
+const wrapperBtn = document.getElementById("wrapper-btn");
 
-console.log(section.hasAttribute("button"));
-console.log(main.setAttribute("title", "new value for main title"));
-// like string
-section.setAttribute("title", "title for section");
+const btns2 = wrapperBtn.children;
 
-//different way
+console.log(btns2);
+
+function handlerBtn(event) {
+  const elem = event.target;
+  const color = elem.dataset.color;
+  console.log(elem.dataset.color);
+  wrapperBtn.style.backgroundColor = color;
+}
 
 
-// like obj
-const attrId = document.createAttribute("id");
-console.dir(attrId);
-attrId.value = "ident-for-section";
-console.log(attrId);
-section.setAttributeNode(attrId)
 
-// dataset
-console.log(section.dataset);
+
+
+
+
+
+
+for (const btn of btns2) {
+  btn.addEventListener("click", handlerBtn);
+}
