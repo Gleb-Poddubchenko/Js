2b957e295df45902966ceea55cedcf93a9435be9
Glebasta <hlibpoddubchenko@gmail.com>
2024-10-13 18:58:07 +0200

    task with setInterval

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 "use strict";
-console.log("1");
+// console.log("1");
 
-const id = setInterval(() => {
-  console.log("subscribe");
-}, 3000);
+// const id = setInterval(() => {
+//   console.log("subscribe");
+// }, 3000);
 
-document.getElementById('btn');
+// document.getElementById('btn');
 
-btn.addEventListener('click',({target})=>{
-    clearInterval(id);
-})
+// btn.addEventListener('click',({target})=>{
+//     clearInterval(id);
+//     console.log('clear');
 
-// clearTimeout(id)
+// })
 
-console.log("2");
+// // clearTimeout(id)
+
+// console.log("2");
+
+function work() {
+  let number = 1;
+
+  let interval = setInterval(() => {
+    console.log(number);
+    number++;
+
+    if (number > 10) clearInterval(interval);
+  }, 500);
+}
+work();
