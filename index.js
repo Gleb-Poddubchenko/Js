8bfe91623fc50ea9041de8b08e9833db3b07ba07
Glebasta <hlibpoddubchenko@gmail.com>
2024-10-13 15:59:18 +0200

    setTimeOut

--- index.js
+++ index.js
@@ -1,67 +1,16 @@
 "use strict";
+console.log("1");
 
-const tasks = [
-  {
-    id: 1,
-    body: "I.I",
-    prioritet: false,
-    option: [],
-    worker: "teamleed",
-  },
-];
+const id = setTimeout(() => {
+  console.log("subscribe");
+}, 3000);
 
-const pattern = /^[А-Я][а-я]{3,15} ([А-Я]\.){2}$/;
+document.getElementById('btn');
 
-const { form } = document.forms;
-const {body:BodyInput} = form.elements;
-
-BodyInput.addEventListener('change', ({target})=>{
-if (pattern.test(target.value)) {
-    target.style.setProperty('background-color', '')
-}
-else{
-    target.style.setProperty('background-color', 'pink')
-}
+btn.addEventListener('click',({target})=>{
+    clearTimeout(id);
 })
 
+// clearTimeout(id)
 
-const ulListTasks = document.createElement("ul");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const { body, prioritet, multiprop, worker } = event.target.elements;
-
-  const optionsArray = [];
-  for (const propElem of multiprop) {
-    if (propElem.checked) {
-      optionsArray.push(propElem.value);
-    }
-  }
-
-  const task = {
-    id: tasks.length + 1,
-    body: body.value,
-    prioritet: prioritet.checked,
-    option: [optionsArray],
-    worker: worker.value,
-  };
-  console.log(task);
-});
-
-// const liTask = document.createElement("li");
-// liTask.innerHTML = `<strong style="color:${tasks.prioritet ? "red" : "blue"}"> ${
-//   tasks.body
-// } <strong/> <em>(${tasks.worker})</em>`;
-// ulListTasks.append(...liTasks);
-
-const liTasks = tasks.map((task) => {
-  const liTask = document.createElement("li");
-  liTask.innerHTML = `<strong style="color:${
-    task.prioritet ? "red" : "blue"
-  }"> ${task.body} <strong/> <em>(${task.worker})</em>`;
-  return liTask;
-});
-ulListTasks.append(...liTasks);
-
-// const{form} =document.forms;
-console.dir(form);
-form.insertAdjacentElement("afterend", ulListTasks);
+console.log("2");
