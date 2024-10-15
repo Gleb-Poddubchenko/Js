a2a3723746e34264b9ffb9e895019bee9d868a88
Glebasta <hlibpoddubchenko@gmail.com>
2024-10-15 19:52:42 +0200

    Add static Promise

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 "use strict";
 
-function loadImage(path = "", alt = "", w = 100, h = 100) {
-  const img = new Image(w, h);
-  img.src = path;
-  img.alt = alt;
-  return new Promise((resolve, reject) => {
-    img.addEventListener("load", () => {
-      resolve(img);
-    });
-    img.addEventListener("error", () => {
-      reject(new Error("path invalid"));
-    });
-  });
+const number = 77;
+
+const numberPromise = Promise.resolve(number);
+
+const handlerPromise =(promise)=>{
+    return promise.then((data)=>{console.log(data);
+    })
 }
 
-loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRizr2lpFUzj3_hy1tkGmVCPJURWOo_IA059A&s')
-.then((elem)=>{
-document.getElementById('root').append(elem)    
-})
-.catch((error)=>console.log(error.message));
+handlerPromise(numberPromise)
