7e070bd5c3c05de6842b3f413e0bf13cd250120d
Glebasta <hlibpoddubchenko@gmail.com>
2024-10-15 19:32:22 +0200

    load img with promise

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 "use strict";
 
- fetch("./users.json").then((response) => 
-  response.json())
-  .then((data) => {
-data.forEach((user)=>{
-    console.log(user.fName);
-    
-})
-  }).catch(()=>{
-    console.table(error);
-  }).finally(()=>{
-    console.table('finally')
-  })
+function loadImage(path = "", alt = "", w = 100, h = 100) {
+  const img = new Image(w, h);
+  img.src = path;
+  img.alt = alt;
+  return new Promise((resolve, reject) => {
+    img.addEventListener("load", () => {
+      resolve(img);
+    });
+    img.addEventListener("error", () => {
+      reject(new Error("path invalid"));
+    });
+  });
+}
 
-// promise.then().catch().finally()
+loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRizr2lpFUzj3_hy1tkGmVCPJURWOo_IA059A&s')
+.then((elem)=>{
+document.getElementById('root').append(elem)    
+})
+.catch((error)=>console.log(error.message));
